7f92c2a3a72fee6be653389c12e15da144d08ce8
Dulfrey Hernández <wdhernandezh@unal.edu.co>
2018-10-21 18:30:39 -0500

    navBar buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { NgModule } from '@angular/core';
 import { AgmCoreModule, GoogleMapsAPIWrapper } from '@agm/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,7 +12,9 @@ import { HomeComponent } from './home/home.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UserSuscribingComponent } from './user-suscribing/user-suscribing.component'; 
-
+import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule } from '@angular/material'; 
+ 
+import { ReactiveFormsModule } from '@angular/forms'; 
 @NgModule({
   declarations: [
     AppComponent,
