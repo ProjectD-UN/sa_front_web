ce8fabc8121503e3a46668cf20c833d76b8779b1
Unknown <diefrodriguezcha@unal.edu.co>
2018-11-07 20:12:28 -0500

    Still working on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,8 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
   }
+
 
 }
